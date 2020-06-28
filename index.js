c36bb1faf998155e30f5aa330825c269a925e58f
Guilherme Bragamonte <guilhermebragamonte@OPAH-000283.local>
2020-06-27 21:37:53 -0300

    @ADD ~ Client events

--- index.js
+++ index.js
@@ -7,18 +7,27 @@ const app = express()
 const server = require('http').Server(app)
 const io = socket(server)
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3001
 const route = require('./app/routes')
 
 app.use(cors())
+
 app.use((req,res,next) => {
   req.io = io
   next()
 })
+
 app.use(bodyParser.json())
 app.use(validator())
 app.use(route)
 
 server.listen(port, () => {
   console.log('Listening on '+port)
+
+  io.on("connection", (socket) => {
+    console.log("New client connected");
+
+    socket.on('guest.new', user => io.emit('guest.show', user));
+    socket.on('message.new', message => io.emit('message.show', message));
+  });
 })
