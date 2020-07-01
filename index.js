a32081d15a77bf22de59d0acc83ded19d0e7a56c
Guilherme Bragamonte <guilhermebragamonte@OPAH-000283.local>
2020-07-01 17:07:01 -0300

    [ADD] ~ Users list and messages history

--- index.js
+++ index.js
@@ -24,10 +24,36 @@ app.use(route)
 server.listen(port, () => {
   console.log('Listening on '+port)
 
+  const users = [];
+  const messages = [];
+
   io.on("connection", (socket) => {
     console.log("New client connected");
-
-    socket.on('guest.new', user => io.emit('guest.show', user));
-    socket.on('message.new', message => io.emit('message.show', message));
+    // Send online users list
+    io.emit('guests.show', users);
+    // Send messages history
+    io.emit('messages.show', messages);
+    // Storage current user to calculate online users
+    var storageUser;
+    //  
+    socket.on('guest.new', user => {
+      io.emit('guest.show', user);
+      users.push(user);
+      storageUser = user;
+    });
+    // 
+    socket.on('message.new', message => {
+      io.emit('message.show', message);
+      messages.push(message);
+    });
+    // 
+    socket.on('disconnect', () => {
+      console.log('Client disconnected:', { storageUser });
+      // Remove user from list
+      if (!storageUser) return;
+      const userIndex = users.findIndex(el => el.name === storageUser.name);
+      if (userIndex > -1) users.splice(userIndex, 1);
+    });
   });
+
 })
